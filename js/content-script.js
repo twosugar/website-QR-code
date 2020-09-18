5c446ee91b389ae9b4c291997b13776e3fa75b8e
tangyu <tangyu@ruhrtec.cn>
2020-09-18 13:44:18 +0800

    fix: 修改二维码生产来源地址

--- js/content-script.js
+++ js/content-script.js
@@ -1,4 +1,4 @@
 ﻿(function() {
   console.log(window.location.href)
-	console.log('这是 simple-chrome-plugin-demo 的content-script！');
+	// console.log('这是 simple-chrome-plugin-demo 的content-script！');
 })();
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,11 @@
 ﻿
 (function() {
   chrome.tabs.getSelected(null, function(tab) {
-    console.log(1, tab.url)
+    // console.log(1, tab.url)
     let img = new Image()
     //生成二维码
-    img.src = `http://qr.topscan.com/api.php?text=${tab.url}`
+    let url = `http://c.wotula.com/chart.php?size=8&level=L&val=${tab.url}`
+    img.src = url || `http://c.wotula.com/chart.php?text=${tab.url}`
     document.getElementsByClassName('chrome-plugin-simple-plugin')[0].appendChild(img)
     document.getElementsByClassName('chrome-plugin-link-box')[0].innerHTML= `<p>当前二维码链接：${tab.url}</p>`
   })
